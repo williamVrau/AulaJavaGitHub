59b4bca9852e5261b2c845598170b0bd1e93baa8
William Mateus <williammnovais@gmail.com>
2025-04-11 19:22:54 -0300

    Atividade 2

---
+++ AtividadeGitHub/src/Exercicio02.java
@@ -0,0 +1,10 @@
+public class Exercicio02 {
+    public static void main(String[] args) {
+        int variavel = 15 ;
+        int variavel1 = 0;
+        for (int i = 1; i <= variavel;i++ ){
+            variavel1 += i;
+       }
+        System.out.println(variavel1);
+    }
+}
