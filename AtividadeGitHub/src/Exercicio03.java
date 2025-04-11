f6aab2efaa80cfd45892e20f5dacc1e609890390
William Mateus <williammnovais@gmail.com>
2025-04-11 19:24:41 -0300

    Atividade 3

---
+++ AtividadeGitHub/src/Exercicio03.java
@@ -0,0 +1,8 @@
+public class Exercicio03 {
+    public static void main(String[] args) {
+        int tabuada = 5;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("" + i + " * " + tabuada + " = " + (i * tabuada));
+        }
+    }
+}
