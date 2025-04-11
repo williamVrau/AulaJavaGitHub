2eb24b5d3981a7ca625b87f03a38900a7bb085fe
William Mateus <williammnovais@gmail.com>
2025-04-11 20:29:01 -0300

    Atividade 15

---
+++ AtividadeGitHub/src/Exercicio15.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercicio15 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        ArrayList<Integer> lista = new ArrayList<>();
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(4);
+        lista.add(5);
+        lista.add(6);
+        lista.add(7);
+        lista.add(8);
+        lista.add(9);
+        lista.add(10);
+        lista.add(11);
+        lista.add(12);
+        lista.add(13);
+        lista.add(15);
+        lista.add(17);
+        int total = 0;
+        for (Integer number :lista){
+            total += number;
+        }
+        System.out.println(total/lista.size());
+    }
+}
