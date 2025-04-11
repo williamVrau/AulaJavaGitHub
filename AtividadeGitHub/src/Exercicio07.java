40e60e1918db7a7bab87265c5f5d1b2ca2e220e0
William Mateus <williammnovais@gmail.com>
2025-04-11 19:39:59 -0300

    Atividade 7

---
+++ AtividadeGitHub/src/Exercicio07.java
@@ -0,0 +1,9 @@
+public class Exercicio07 {
+    public static void main(String[] args) {
+        int varial = 123;
+        int varial1 = varial/100;
+        int varial2 = (varial%100)/10;
+        int varial3 = varial%10;
+        System.out.println(varial1+" + "+varial2+" + "+varial3+" = "+(varial1+varial2+varial3));
+    }
+}
