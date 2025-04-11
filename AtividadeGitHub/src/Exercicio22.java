90c20606d48092c4e7d418fdf0a97e8cb5d96260
William Mateus <williammnovais@gmail.com>
2025-04-11 20:47:44 -0300

    Atividade 21

---
+++ AtividadeGitHub/src/Exercicio22.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercicio22 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        ArrayList<Integer> lista = new ArrayList<>();
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(4);
+        lista.add(5);
+        lista.add(6);
+        lista.add(7);
+        lista.add(8);
+        lista.add(9);
+        lista.add(10);
+        lista.add(11);
+        lista.add(12);
+        lista.add(13);
+        lista.add(15);
+        lista.add(17);
+        ArrayList<Integer> newList = new ArrayList<>();
+        lista.add(1);
+        lista.add(2);
+        lista.add(5);
+        lista.add(6);
+    }
+}
